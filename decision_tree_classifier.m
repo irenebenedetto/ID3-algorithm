4c78bd5d8edb663590a02a8e9ffead30af118a4f
irenebenedetto <ire.benedetto@gmail.com>
2020-09-23 10:50:43 +0200

    changed function

--- decision_tree_classifier.m
+++ decision_tree_classifier.m
@@ -1,19 +1,10 @@
-function decision =  decision_tree_classifier(v)
+function decision =  decision_tree_classifier(v, train_set, class_label, numerical)
 
 fprintf('*** DECISION TREE CLASSIFIER ***\n\n');
 
-train_set = [30, 0, 10;      %0
-             30, 0, 70;      %0
-             30, 1, 20;      %0
-             30, 1, 80;      %1
-             60, 0, 40;      %0
-             60, 0, 60;      %1
-             60, 1, 50;      %0
-             60, 1, 60;];    %1
+
          
 disp(train_set);
-numerical = [1 3];
-class_label = [0;0;0;1;0;1;0;1];
 disp(class_label);
 
 % alpha is a vector that contains the alphabeth of the class (0, 1)
--- main.m
+++ main.m
@@ -1,6 +1,17 @@
 clear all;
 clc;
+train_set = [30, 0, 10;      %0
+             30, 0, 70;      %0
+             30, 1, 20;      %0
+             30, 1, 80;      %1
+             60, 0, 40;      %0
+             60, 0, 60;      %1
+             60, 1, 50;      %0
+             60, 1, 60;];    %1
+         
+class_label = [0;0;0;1;0;1;0;1];
+numerical = [1 3];
 
-decision = decision_tree_classifier([14 1 122]);
+decision = decision_tree_classifier([14 1 122], train_set, class_label, numerical);
 
 decision
